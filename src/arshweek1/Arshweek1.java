9f33289bd8bcb2448c5836010691ce3ed655ecde
Arsh234 <singlaar@sheridancollege.ca>
2019-06-18 16:45:05 -0400

    Student mai gand pata

---
+++ src/arshweek1/Arshweek1.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arshweek1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Ankush
+ */
+public class Arshweek1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String[] str = new String[5];
+        System.out.println("enter the 5 elements");
+        for(int i=str.length-1;i>=0;i--)
+        {
+            Scanner sc = new Scanner(System.in);
+            str[i]=sc.next();
+            
+        }
+        for(String e: str )
+        {
+            System.out.println("The elements are:"+e);
+                
+        }
+        
+    }
+    
+}
